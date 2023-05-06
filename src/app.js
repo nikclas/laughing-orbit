5a06f8eda3da31e5bdd0cc21a08b81fe527936d4
nikclas <130708102+nikclas@users.noreply.github.com>
2023-05-06 02:03:32 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
+let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+let when = [
+  "antes de clases",
+  "justo cuando terminé",
+  "mientras almorzaba",
+  "mientras yo rezaba"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(qui, que, cuand) {
+  numQuien = Math.floor(Math.random() * qui.length);
+  numQue = Math.floor(Math.random() * que.length);
+  numCuando = Math.floor(Math.random() * cuand.length);
+  return (
+    qui[numQuien] +
+    " " +
+    que[numQue] +
+    " " +
+    "mi tarea" +
+    " " +
+    cuand[numCuando]
+  );
+}
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, what, when);
+}
+window.onload = onLoad;
